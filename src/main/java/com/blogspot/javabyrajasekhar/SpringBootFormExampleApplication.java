4e5f9883bdb7b5ee39d3e874cb6d86146b72d83b
talk2rajasekhar <talk2rajasekhar>
2019-02-14 10:39:24 +0530

    initial comit

---
+++ src/main/java/com/blogspot/javabyrajasekhar/SpringBootFormExampleApplication.java
@@ -0,0 +1,14 @@
+package com.blogspot.javabyrajasekhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFormExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFormExampleApplication.class, args);
+	}
+
+}
+
